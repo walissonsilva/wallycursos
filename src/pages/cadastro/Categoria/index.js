7dcf76e3626ed49c9d2620d16f7154c91ff70fc4
Walisson Silva <walissonsilva10@gmail.com>
2020-07-30 17:48:50 -0300

    Adição do Slider nos vídeos

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,9 +6,24 @@ function Categoria() {
 	return (
 		<PageDefault>
 			<h1>Cadastro de Categoria</h1>
-			
-			<Link to='/'>
-				Voltar para a Home
+
+			<form>
+
+				<label>
+				  Nome da Categoria:
+				  <input
+				    type="text"
+				  />
+				</label>
+
+				<button>
+				  Cadastrar
+				</button>
+			</form>
+
+
+			<Link to="/">
+				Ir para home
 			</Link>
 		</PageDefault>
 	)
